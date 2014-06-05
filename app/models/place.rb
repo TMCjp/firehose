a26f7a2f58b9559b3b9263ebe4d7fa2f80cd0ed6
vagrant <vagrant@precise32.(none)>
2014-06-05 01:40:36 +0000

    added validation for name, address, and description

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :uniqueness => true, :length => { :within => 3..100 }
+	validates :address, :presence => true, :length => { :within => 1..100 }
+	validates :description, :presence => true, :length => { :within => 1..400 }
 end
